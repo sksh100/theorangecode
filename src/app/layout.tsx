b483f2bf789160f366c8bbae8e810124819c6f09
sksh100 <sksh100@users.noreply.github.com>
2025-11-03 01:57:31 +0400

    Apply Glacial Indifference font to all text on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,9 +39,9 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Glacial+Indifference:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body style={{ fontFamily: "'Inter', sans-serif" }} className="antialiased">
+      <body style={{ fontFamily: "'Glacial Indifference', sans-serif" }} className="antialiased">
         {children}
       </body>
     </html>
